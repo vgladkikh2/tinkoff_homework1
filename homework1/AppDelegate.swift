b2cefefc2c9e508772de6d857681cb8df2e4f6ef
me <me@macbook-air-me.dlink>
2018-09-24 23:56:51 +0300

    Improved log

--- homework1/AppDelegate.swift
+++ homework1/AppDelegate.swift
@@ -9,13 +9,34 @@
 import UIKit
 import CoreData
 
+extension UIApplicationState {
+    var textualDescription: String {
+        switch self {
+        case .active:
+            return "active"
+        case .inactive:
+            return "inactive"
+        case .background:
+            return "background"
+        }
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    #if PRINTLOGS
+    static var lastApplicationState : UIApplicationState?
+    #endif
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        #if PRINTLOGS
+        print("Application moved from not running to \(UIApplication.shared.applicationState.textualDescription): \(#function)")
+        AppDelegate.lastApplicationState = UIApplication.shared.applicationState
+        #endif
         // Override point for customization after application launch.
         return true
     }
@@ -24,30 +45,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         #if PRINTLOGS
-            print("Application moved from foreground-active to foreground-inactive: \(#function)")
+        print("Application is about to move from \(UIApplication.shared.applicationState.textualDescription) to \(UIApplicationState.inactive.textualDescription): \(#function)")
+        AppDelegate.lastApplicationState = UIApplicationState.inactive
         #endif
-        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         #if PRINTLOGS
-            print("Application moved from foreground-inactive to background: \(#function)")
+        print("Application moved from \(AppDelegate.lastApplicationState!.textualDescription) to \(UIApplication.shared.applicationState.textualDescription): \(#function)")
+        AppDelegate.lastApplicationState = UIApplication.shared.applicationState
         #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         #if PRINTLOGS
-            print("Application moved from background to foreground-inactive: \(#function)")
+        print("Application is about to move from \(UIApplication.shared.applicationState.textualDescription) to \(UIApplicationState.inactive.textualDescription): \(#function)")
+        AppDelegate.lastApplicationState = UIApplicationState.inactive
         #endif
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         #if PRINTLOGS
-            print("Application moved from foreground-inactive to foreground-active: \(#function)")
+        print("Application moved from \(AppDelegate.lastApplicationState!.textualDescription) to \(UIApplication.shared.applicationState.textualDescription): \(#function)")
+        AppDelegate.lastApplicationState = UIApplication.shared.applicationState
         #endif
     }
 
@@ -55,7 +79,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         #if PRINTLOGS
-            print("Application is about to terminate: \(#function)")
+        print("Application is about to move from \(UIApplication.shared.applicationState.textualDescription) to terminate: \(#function)")
         #endif
         self.saveContext()
     }
