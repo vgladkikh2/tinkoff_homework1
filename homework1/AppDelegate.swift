c8bc9e4d0aad37d9152b121d764e232edf2ee6d3
me <me@macbook-air-me.dlink>
2018-09-24 08:56:57 +0300

    Added log of AppDelegate lifecycle Added log when running ViewController method Added .gitignore

--- homework1/AppDelegate.swift
+++ homework1/AppDelegate.swift
@@ -23,24 +23,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        #if PRINTLOGS
+            print("Application moved from foreground-active to foreground-inactive: \(#function)")
+        #endif
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        #if PRINTLOGS
+            print("Application moved from foreground-inactive to background: \(#function)")
+        #endif
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        #if PRINTLOGS
+            print("Application moved from background to foreground-inactive: \(#function)")
+        #endif
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        #if PRINTLOGS
+            print("Application moved from foreground-inactive to foreground-active: \(#function)")
+        #endif
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
+        #if PRINTLOGS
+            print("Application is about to terminate: \(#function)")
+        #endif
         self.saveContext()
     }
 
--- homework1/ViewController.swift
+++ homework1/ViewController.swift
@@ -19,7 +19,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated:Bool) {
+        super.viewWillAppear(animated)
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
+    
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
+    
+    override func viewWillDisappear(_ animated:Bool) {
+        super.viewWillDisappear(animated)
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
+    
+    override func viewDidDisappear(_ animated:Bool) {
+        super.viewDidDisappear(animated)
+        #if PRINTLOGS
+            print(#function)
+        #endif
+    }
 
 }
 
